ebc5b6d80ea055c51f26354af73a494feb75a505
AngryMexDog <aortiz19.depad.sj@gmail.com>
2023-09-11 21:00:53 -0600

    Add second calculation. New bug added

--- main.py
+++ main.py
@@ -1,13 +1,4 @@
 import os
-
-
-
-
-
-
-
-
-
 def add(n1, n2):
   return n1 + n2
 
@@ -33,9 +24,16 @@ num2 = int(input("What's the second number?: "))
 for symbol in operations:
     print(symbol)
 
-operation_symbol = input("Pick and operation from the line above: ")
+operation_symbol = input("Pick an operation from the line above: ")
 
 calculation_function = operations[operation_symbol]
-answer = calculation_function(num1, num2)
+first_answer = calculation_function(num1, num2)
 
-print(f"{num1} {operation_symbol} {num2} = {answer}")
+print(f"{num1} {operation_symbol} {num2} = {first_answer}")
+
+operation_symbol = input("Pick another operation from the line above: ")
+num3 = int(input("What's the next number?: "))
+calculation_function = operations[operation_symbol]
+second_answer = calculation_function(calculation_function(num1, num2),num3)
+
+print(f"{first_answer} {operation_symbol} {num3} = {second_answer}")
